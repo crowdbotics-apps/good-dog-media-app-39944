c7a6a91acb47ca67be721a30824d75212a57a710
Crowdbotics <team@crowdbotics.com>
2023-04-14 18:39:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "good-dog-media-app-39944",
+  url: "https://good-dog-media-app-39944.botics.co",
+  api: "https://good-dog-media-app-39944.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
